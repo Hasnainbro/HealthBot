f5834f853e44c6037287bbdd35a5800ba0229f91
anuraghatole <anuraghatole123@gmail.com>
2024-08-19 17:00:10 +0530

    integrated functions to call the backend apis for login and register

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const URL = "";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const responce = await axios.post(URL, {
+        email,
+        password,
+      });
+
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log("error occoured will log in", error);
+    }
+  };
+
   return (
     <div className="h-full w-full mt-28">
       <div className="flex justify-end"></div>
@@ -13,35 +35,48 @@ const Login = () => {
       </div>
 
       <div className="mt-14">
-        <div className="flex flex-col items-center">
-          <div className="w-60">
-            <label className="font-medium text-lg mb-2">Email:</label>
-            <input
-              type="text"
-              className="border border-zinc-500 py-2 rounded-sm w-80"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center mt-4">
-          <div className="w-60">
-            <label className="font-medium text-lg mb-2">Password:</label>
-            <input
-              type="password"
-              className="border border-zinc-500 py-2 rounded-sm w-80"
-            />
-            <div className="flex justify-between items-center mt-5 w-80">
-              <button className=" text-lg mt-5 bg-black text-white px-4 py-2 border-white rounded-md">
-                Sign In
-              </button>
-              <p className="text-sm mt-4 font-medium">
-                Don't have an account?{" "}
-                <Link to="/register" className="text-blue-500">
-                  Register
-                </Link>
-              </p>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col items-center">
+            <div className="w-60">
+              <label className="font-medium text-lg mb-2">Email:</label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                className="border border-zinc-500 py-2 rounded-sm w-80 text-lg font-normal pl-3"
+              />
             </div>
           </div>
-        </div>
+          <div className="flex flex-col items-center mt-4">
+            <div className="w-60">
+              <label className="font-medium text-lg mb-2">Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="border border-zinc-500 py-2 rounded-sm w-80 text-lg font-normal pl-3"
+              />
+              <div className="flex justify-between items-center mt-5 w-80">
+                <button
+                  type="submit"
+                  className=" text-lg mt-5 bg-black text-white px-4 py-2 border-white rounded-md"
+                >
+                  Sign In
+                </button>
+                <p className="text-sm mt-4 font-medium">
+                  Don't have an account?{" "}
+                  <Link to="/register" className="text-blue-500">
+                    Register
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
--- Client/src/components/Register.jsx
+++ Client/src/components/Register.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const URL = "";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const responce = await axios.post(URL, {
+        name,
+        email,
+        password,
+      });
+      alert("Registration Successfull!!");
+
+      setName("");
+      setEmail("");
+      setPassword("");
+
+      navigate("/login");
+    } catch (error) {
+      console.log("error occoured will registring user", error);
+    }
+  };
+
   return (
     <div>
       <div className="h-full w-full  mt-44 ">
@@ -10,41 +39,55 @@ const Register = () => {
         </div>
 
         <div className="mt-14">
-          <div className="flex flex-col items-center">
-            <div className="w-60">
-              <label className="font-medium text-lg mb-2">Username:</label>
-              <input
-                type="text"
-                className="border border-zinc-500 py-2 rounded-sm w-80"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="w-60">
-              <label className="font-medium text-lg mb-2">Email:</label>
-              <input
-                type="text"
-                className="border border-zinc-500 py-2 rounded-sm w-80"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col items-center mt-4">
-            <div className="w-60">
-              <label className="font-medium text-lg mb-2">Password:</label>
-              <input
-                type="password"
-                className="border border-zinc-500 py-2 rounded-sm w-80"
-              />
-              <div className=" flex justify-center ml-20 mt-1">
-                {" "}
-                <Link to={"/login"}>
-                  <button className=" mt-5 text-lg bg-black text-white px-4 py-2 border-white rounded-md">
-                    Register
-                  </button>
-                </Link>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col items-center">
+              <div className="w-60">
+                <label className="font-medium text-lg mb-2">Username:</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  className="border border-zinc-500 py-2 rounded-sm w-80 text-lg font-normal pl-3"
+                />
               </div>
             </div>
-          </div>
+            <div className="flex flex-col items-center">
+              <div className="w-60">
+                <label className="font-medium text-lg mb-2">Email:</label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  className="border border-zinc-500 py-2 rounded-sm w-80 text-lg font-normal pl-3"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col items-center mt-4">
+              <div className="w-60">
+                <label className="font-medium text-lg mb-2">Password:</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  className="border border-zinc-500 py-2 rounded-sm w-80 text-lg font-normal pl-3"
+                />
+                <div className=" flex justify-center ml-20 mt-1">
+                  <button
+                    type="submit"
+                    className=" mt-5 text-lg bg-black text-white px-4 py-2 border-white rounded-md"
+                  >
+                    Register
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
